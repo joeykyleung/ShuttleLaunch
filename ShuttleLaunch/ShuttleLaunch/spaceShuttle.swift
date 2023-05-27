a4190ff28bd7eb74ca5750327808d2e8cfdc9ce8
Joey Leung <77413460+joeykyleung@users.noreply.github.com>
2023-05-27 17:06:14 -0400

    Add files via upload

---
+++ ShuttleLaunch/ShuttleLaunch/spaceShuttle.swift
@@ -0,0 +1,56 @@
+//
+//  spaceShuttle.swift
+//  ShuttleLaunch
+//
+//  Created by Joey Leung on 25/5/2016.
+//  Copyright © 2016 Joey Leung. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class spaceShuttle: SKSpriteNode{
+    
+    var vector : CGVector
+    var rect: CGRect
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init (imageNamed: String){
+        
+        let imageTexture = SKTexture(imageNamed: imageNamed)
+        vector = CGVectorMake(0, 0)
+        rect = CGRectMake(0,0,0,0)
+        super.init(texture: imageTexture, color:UIColor.clear, size: imageTexture.size())
+        
+        self.physicsBody = SKPhysicsBody(rectangleOf: imageTexture.size())
+        self.physicsBody?.isDynamic = false
+        self.physicsBody?.allowsRotation = false
+        
+        rect = CGRectMake(self.frame.midX, self.frame.midY, self.frame.width, self.frame.height)
+    }
+    
+    init(imageNamed: String, no: Int){
+        let imageTexture = SKTexture(imageNamed: imageNamed)
+        vector = CGVectorMake(0, 0)
+        rect = CGRectMake(0,0,0,0)
+        super.init(texture: imageTexture, color:UIColor.clear, size: imageTexture.size())
+        
+        self.physicsBody = SKPhysicsBody(rectangleOf: imageTexture.size())
+        self.physicsBody?.isDynamic = false
+        self.physicsBody?.allowsRotation = false
+        
+        rect = CGRectMake(self.frame.midX, self.frame.midY, self.frame.width, self.frame.height)
+    }
+    func move(){
+        self.physicsBody?.applyImpulse(vector)
+    }
+    func moveShuttle(touch:UITouch){
+        let force = (touch.force > 1 ? touch.force : 1)
+        
+        vector = CGVectorMake(0, force/4*3)
+        self.physicsBody?.applyImpulse(vector)
+    }
+}
